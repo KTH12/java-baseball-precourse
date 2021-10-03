b31ee612921c9c2fe84cb422b905d4d4697da168
slow <irooori89@gmail.com>
2021-10-03 21:50:44 +0900

    feat(BaseBallResultServiceImpl) : 스트라이크, 볼 검증 구현
    - BaseBallResultServiceImpl@strikeAndBallChecker 함수를 통해 BaseBallResult를 리턴합니다.
      BaseBallResult는  매칭된 스타라이크 카운트와, 볼 카운트를 가지고 있습니다.

--- src/main/java/domain/BaseBallResultService.java
+++ src/main/java/domain/BaseBallResultService.java
@@ -1,4 +1,5 @@
 package domain;
 
 public interface BaseBallResultService {
+	BaseBallResult strikeAndBallChecker();
 }
--- src/main/java/infrastructure/BaseBallResultServiceImpl.java
+++ src/main/java/infrastructure/BaseBallResultServiceImpl.java
@@ -1,4 +1,53 @@
 package infrastructure;
 
-public class BaseBallResultServiceImpl {
+import domain.BaseBallResult;
+import domain.BaseBallResultService;
+
+import java.util.List;
+
+public class BaseBallResultServiceImpl implements BaseBallResultService {
+	BaseBallResult baseBallResult;
+	List<Integer> computerBalls;
+	List<Integer> userBalls;
+
+	public BaseBallResultServiceImpl(List<Integer> computerBalls, List<Integer> userBalls) {
+		this.baseBallResult = new BaseBallResult();
+		this.computerBalls = computerBalls;
+		this.userBalls = userBalls;
+	}
+
+	@Override public BaseBallResult strikeAndBallChecker() {
+		zoneValid(computerBalls, userBalls);
+		return getBaseBallResult();
+	}
+
+	private void zoneValid(List<Integer> computerBalls, List<Integer> userBalls) {
+		for (int i = 0; i < computerBalls.size(); i++) {
+			zoneMatch(computerBalls.get(i), userBalls.get(i));
+		}
+	}
+
+	private void zoneMatch(Integer computerBallNumber, Integer userBallNumber) {
+		if (isStrike(computerBallNumber, userBallNumber)) {
+			baseBallResult.incrementStrikeCount();
+			return;
+		}
+
+		if (isBall(userBallNumber)) {
+			baseBallResult.incrementBallCount();
+		}
+	}
+
+	private boolean isBall(Integer userBallNumber) {
+		return computerBalls.contains(userBallNumber);
+	}
+
+	private boolean isStrike(Integer computerBallNumber, Integer userBallNumber) {
+		return computerBallNumber.equals(userBallNumber);
+	}
+
+	private BaseBallResult getBaseBallResult() {
+		return baseBallResult;
+	}
+
 }
