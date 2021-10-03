a40ddc4d86fb5d098c6f407077e7ab8a7ae150e7
slow <irooori89@gmail.com>
2021-10-03 22:38:31 +0900

    feat(BaseBallGame) : BaseBallGame.java 추가 야구 게임 베이스 서비스
    - 컴퓨터 랜덤 숫자 추가
    - 유저 입력을 받고 게임을 진행 추가
    - 게임완료시 로직 종료

--- src/main/java/application/BaseBallGame.java
+++ src/main/java/application/BaseBallGame.java
@@ -1,4 +1,53 @@
 package application;
 
+import console.Output;
+import domain.BaseBallResult;
+import domain.BaseBallService;
+import domain.BaseBallServiceImpl;
+import nextstep.utils.Console;
+
 public class BaseBallGame {
+	private final BaseBallService baseBallService = new BaseBallServiceImpl();
+	private final Integer COMPLETE_STRIKE_COUNT = 3;
+
+	public void play() {
+		baseBallService.generateComputerInfo();
+
+		do {
+			gameProcess();
+		} while (!isSuccess());
+	}
+
+	private boolean isSuccess() {
+		BaseBallResult baseBallResult = baseBallService.isSuccess();
+		printBallMatchResult(baseBallResult.getStrikeCount(), baseBallResult.getBallCount());
+
+		return baseBallResult.getStrikeCount() == COMPLETE_STRIKE_COUNT;
+	}
+
+	private void gameProcess() {
+		try {
+			Output.printRequireNumberMessage();
+			String userInput = Console.readLine();
+			baseBallService.generateUserInfo(userInput);
+		} catch (IllegalArgumentException e) {
+			Output.printExceptionMessage(e.getMessage());
+			gameProcess();
+		}
+	}
+
+	private void printBallMatchResult(int strikeCount, int ballCount) {
+		if (strikeCount == COMPLETE_STRIKE_COUNT) {
+			Output.printMessage(strikeCount + Output.STRIKE_MESSAGE);
+			Output.printGameComplete();
+			return;
+		}
+
+		if (strikeCount == 0 && ballCount == 0) {
+			Output.printNothing();
+			return;
+		}
+
+		Output.printMessage(strikeCount + Output.STRIKE_MESSAGE + ballCount + Output.BALL_MESSAGE);
+	}
 }
