42aac8e358623adb600ad216abf2f02217aa4b4a
slow <irooori89@gmail.com>
2021-10-03 21:58:11 +0900

    feat(GameFacade) : 스트라이크,볼 검증 추가
    - 야구 게임 실행 추가
    - 유저 계속 진행 여부 체크 추가

--- src/main/java/application/GameFacade.java
+++ src/main/java/application/GameFacade.java
@@ -1,4 +1,21 @@
 package application;
 
+import console.Output;
+import nextstep.utils.Console;
+
 public class GameFacade {
+
+	public void run() {
+		do {
+			BaseBallGame baseBallGame = new BaseBallGame();
+			baseBallGame.play();
+		} while (isCorrectContinueQuestions());
+	}
+
+	private boolean isCorrectContinueQuestions() {
+		String CONiNUE_MESSAGE = "1";
+		Output.printContinueMessage();
+		return Console.readLine().equals(CONiNUE_MESSAGE);
+	}
+
 }
--- src/main/java/console/Output.java
+++ src/main/java/console/Output.java
@@ -1,4 +1,9 @@
 package console;
 
 public class Output {
+	private static final String CONTINUE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+	public static void printContinueMessage() {
+		System.out.println(CONTINUE_MESSAGE);
+	}
 }
