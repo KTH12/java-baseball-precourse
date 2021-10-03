46ba9eee5c9dac622a0f4242f9986b3b8ef7404a
slow <irooori89@gmail.com>
2021-10-03 17:19:17 +0900

    feat(Ball) : Ball 기능 구현
    - number 변수 지정
    - 1부터 9 이외의 값 에러 처리 함수 추가(validBallNumber)

--- src/main/java/domain/Ball.java
+++ src/main/java/domain/Ball.java
@@ -1,4 +1,23 @@
 package domain;
 
 public class Ball {
+	private static final int MIN = 1;
+	private static final int MAX = 9;
+
+	private final int number;
+
+	public Ball(int number) {
+		validBallNumber(number);
+		this.number = number;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	private void validBallNumber(int number) {
+		if (number < MIN || number > MAX) {
+			throw new IllegalArgumentException("1부터 9까지 숫자만 입력 할 수 있습니다.");
+		}
+	}
 }
