1f7facb3ff447970c92b2f50fdff3cf6e73782ea
slow <irooori89@gmail.com>
2021-10-04 12:04:04 +0900

    refactoring(Exception): Exception 관리 모듈 정의
     - BaseException.java 추가
     - InvalidParamException.java 추가

---
+++ src/main/java/common/response/exception/InvalidParamException.java
@@ -0,0 +1,15 @@
+package common.response.exception;
+
+import common.response.ErrorCode;
+
+public class InvalidParamException extends BaseException {
+
+    public InvalidParamException() {
+        super(ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+
+    public InvalidParamException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+}
